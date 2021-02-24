bfa1f04ed37e26c76cf8edf615aebcb1f6fbcf9d
Frank Merchlewitz <jlimhb@gmail.com>
2021-02-24 17:41:57 -0600

    starting to do inquirer using old code

--- app.js
+++ app.js
@@ -33,3 +33,78 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+const employees = [];
+
+const introQs = [
+    {
+      type: 'input',
+      name: 'project',
+      message: 'Greetings, Project Manager! Enter your name?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Briefly describe what your project does.'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What installations are required for your project?'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How does one use your project?'
+    },
+    {
+      type: 'input',
+      name: 'contributors',
+      message: 'Please list other contributors to this project if there are any.'
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'How does one run tests on this project?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What type of license does this project have?',
+        choices: ['Apache 2.0', 'GNU', 'MIT', 'Creative Commons', 'None']
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?'
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+      },
+  ];
+
+// TODO: Create a function to write README file
+  function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Success! ReadMe Written')
+    );
+}
+
+// Function call to initialize app
+
+// init function with one argument which will be our questions array
+function init(prompts) {
+  // tell inquirer to run that questions array into the terminal
+    inquirer.prompt(prompts)
+        .then((answers) => {
+    
+        // Take what we are storing in newReadMe and write it. If there's an error, return the error, if not alert the console the file was written successfully
+
+            const newREADME = markdown(answers);
+            // function we made earlier
+            writeToFile("newREADME.md", newREADME);
+        })
+}
+init(questions);
